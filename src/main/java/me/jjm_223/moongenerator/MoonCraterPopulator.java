dd2a91734d158e453ea5fb3950340cc8785eef50
jjm223 <jjm_223@hotmail.com>
2016-09-24 11:26:56 -0500

    Add comments.

--- src/main/java/me/jjm_223/moongenerator/MoonCraterPopulator.java
+++ src/main/java/me/jjm_223/moongenerator/MoonCraterPopulator.java
@@ -37,6 +37,7 @@ public class MoonCraterPopulator extends BlockPopulator
             {
                 radiusBase = random.nextInt(BIG_CRATER_SIZE - MIN_CRATER_SIZE + 1) + MIN_CRATER_SIZE;
                 radiusX = radiusBase + random.nextInt(MAX_RADIUS_VARIANCE);
+                // Craters on the moon are flatter than they are wide.
                 radiusY = (int) (radiusBase * 0.7);
                 radiusZ = radiusBase + random.nextInt(MAX_RADIUS_VARIANCE);
             }
@@ -48,14 +49,17 @@ public class MoonCraterPopulator extends BlockPopulator
                 radiusZ = radiusBase + random.nextInt(MAX_RADIUS_VARIANCE);
             }
 
+            // Add 0.5 to the radius to improve the shape of the ellipsoid.
             radiusX += 0.5;
             radiusY += 0.5;
             radiusZ += 0.5;
 
+            // Create inverse radii squared. Doing the calculations back here and using the inverse for optimization.
             double irx = 1.0 / (radiusX * radiusX);
             double iry = 1.0 / (radiusY * radiusY);
             double irz = 1.0 / (radiusZ * radiusZ);
 
+            // Round the radii up to iterate through them all.
             int radXCeil = (int) Math.ceil(radiusX);
             int radYCeil = (int) Math.ceil(radiusY);
             int radZCeil = (int) Math.ceil(radiusZ);
@@ -66,8 +70,10 @@ public class MoonCraterPopulator extends BlockPopulator
                 {
                     for (int cz = -radZCeil; cz < radZCeil; cz++)
                     {
+                        // Test if block is within the sphere.
                         if (validBlock(cx, cy, cz, irx, iry, irz))
                         {
+                            // Skip some blocks to add imperfections to the crater.
                             if (!(outerBlock(cx, cy, cz, irx, iry, irz)
                                     && random.nextInt(100) <= IGNORE_OUTER_BLOCK_CHANCE))
                             {
@@ -80,6 +86,17 @@ public class MoonCraterPopulator extends BlockPopulator
         }
     }
 
+    /**
+     * Tests if coordinate is within an ellipsoid.
+     *
+     * @param x x coordinate to test
+     * @param y y coordinate to test
+     * @param z z coordinate to test
+     * @param irx inverse of the x radius squared
+     * @param iry inverse of the y radius squared
+     * @param irz inverse of the z radius squared
+     * @return true if block is within the ellipsoid
+     */
     private boolean validBlock(int x, int y, int z, double irx, double iry, double irz)
     {
         double result = ((x * x) * irx) + ((y * y) * iry) + ((z * z) * irz);
@@ -87,6 +104,17 @@ public class MoonCraterPopulator extends BlockPopulator
         return result < 1;
     }
 
+    /**
+     * Tests if coordinate is on the outer edge of an ellipsoid.
+     *
+     * @param x x coordinate to test
+     * @param y y coordinate to test
+     * @param z z coordinate to test
+     * @param irx inverse of the x radius squared
+     * @param iry inverse of the y radius squared
+     * @param irz inverse of the z radius squared
+     * @return true if block is on the outer edge of the ellipsoid.
+     */
     private boolean outerBlock(int x, int y, int z, double irx, double iry, double irz)
     {
         double result = ((x * x) * irx) + ((y * y) * iry) + ((z * z) * irz);
